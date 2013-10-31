3be68591ebec79c1259b8ff944de7be34f12fbcb
Rahul Revo <rahulrevo@gmail.com>
2013-10-31 11:13:57 -0700

    Minor comments

--- src/main/java/app/UuidType.java
+++ src/main/java/app/UuidType.java
@@ -12,22 +12,19 @@ import org.hibernate.MappingException;
 import org.hibernate.engine.spi.SessionImplementor;
 import org.hibernate.usertype.UserType;
 
+/**
+ * Serialize instances of {@link UUID} via JDBC
+ *
+ */
 public class UuidType implements UserType {
 
+   /*
+    * Return the SQL type codes for the columns mapped by this type. The
+    * codes are defined on <tt>java.sql.Types</tt>.
+    */
    @Override
-   public Object assemble(Serializable cached, Object owner) throws HibernateException {
-      return cached;
-   }
-
-   @Override
-   public Object deepCopy(Object o) throws HibernateException {
-      // UUID is immutable
-      return o;
-   }
-
-   @Override
-   public Serializable disassemble(Object o) throws HibernateException {
-      return (Serializable) o;
+   public int[] sqlTypes() {
+      return new int[] { Types.OTHER };
    }
 
    @Override
@@ -47,27 +44,15 @@ public class UuidType implements UserType {
       return o.hashCode();
    }
 
-   @Override
-   public boolean isMutable() {
-      return false;
-   }
-
-   @Override
-   public Object replace(Object original, Object target, Object owner) throws HibernateException {
-      return original;
-   }
-
    @SuppressWarnings("rawtypes")
    @Override
    public Class returnedClass() {
       return UUID.class;
    }
 
-   @Override
-   public int[] sqlTypes() {
-      return new int[] { Types.OTHER };
-   }
-
+   /*
+    * Retrieve an instance of the mapped class from a JDBC resultset.
+    */
    @Override
    public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner)
          throws HibernateException, SQLException {
@@ -79,9 +64,38 @@ public class UuidType implements UserType {
       }
    }
 
+   /*
+    * Write an instance of the mapped class to a prepared statement.
+    */
    @Override
    public void nullSafeSet(PreparedStatement st, Object value, int index,
          SessionImplementor session) throws HibernateException, SQLException {
       st.setObject(index, value);
+   }
+
+   @Override
+   public Object deepCopy(Object o) throws HibernateException {
+      // UUID is immutable
+      return o;
+   }
+
+   @Override
+   public boolean isMutable() {
+      return false;
+   }
+
+   @Override
+   public Serializable disassemble(Object o) throws HibernateException {
+      return (Serializable) o;
+   }
+
+   @Override
+   public Object assemble(Serializable cached, Object owner) throws HibernateException {
+      return cached;
+   }
+
+   @Override
+   public Object replace(Object original, Object target, Object owner) throws HibernateException {
+      return original;
    }
 }
